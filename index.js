78a54614ea0a6d6b00407129523d3e6e8a9be866
Maverick Vitto <mavvitto@yahoo.com>
2023-10-31 19:41:04 -0600

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31 
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random()*10) +1
+
+let num3 = 12
+let num4 = 3
+let mod = num3/num4
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
